e1134f55dbb1c94fb9b9140a50202980759d5a13
Alim ÖZDEMİR <alm.ozdmr@gmail.com>
2024-11-08 18:56:37 +0100

    fix: windows problem

--- src/utils/nuxt.ts
+++ src/utils/nuxt.ts
@@ -1,10 +1,10 @@
-import { posix, extname } from "path";
+import { posix } from "path";
 import { kebabCase } from "scule";
 
 export function nuxtLayoutPathToKey(layoutPath: string) {
   // Normalize the path to ensure consistency across different operating systems
-  const normalizedPath = posix.normalize(layoutPath);
-
+  const normalizedPath = posix.normalize(layoutPath).replace(/\\/g, '/');
+  
   const pathWithoutExt = normalizedPath.slice(1, -posix.extname(normalizedPath).length);
 
   return kebabCase(pathWithoutExt);
